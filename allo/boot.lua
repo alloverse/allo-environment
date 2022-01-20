67bc4baa5cb52c826acaa5eddcb8aff31b45c553
Nevyn Bengtsson <nevyn@alloverse.com>
2022-01-20 10:49:26 +0100

    bump assist and alloui and allonet

--- allo/boot.lua
+++ allo/boot.lua
@@ -18,13 +18,16 @@ function os.uname()
     return os.system("uname -s")
 end
 
+local dylibext = ""
 if os.uname():find("^Darwin") ~= nil then
-    package.cpath = package.cpath..";"..libDir.."/osx64/?.dylib"
+    dylibext = "dylib"
 elseif string.match(package.cpath, "so") then
-    package.cpath = package.cpath..";"..libDir.."/linux64/?.so"
+    dylibext = "so"
 elseif string.match(package.cpath, "dll") then
-    package.cpath = package.cpath..";"..libDir.."/win64/?.dll"
+    dylibext = "dll"
 end
+
+package.cpath = package.cpath..";"..libDir.."/?."..dylibext
 
 package.path = package.path
     ..";"..srcDir.."/?.lua"
@@ -34,11 +37,41 @@ package.path = package.path
     
 -- Establish globals
 require("liballonet")
+local ffi = require 'ffi'
+local libav_available, av = pcall(ffi.load, libDir .. "/liballonet_av."..dylibext, true)
+if not libav_available then
+    av = nil
+    print("NOTE: liballonet_av not available, h264 cannot be used")
+else
+    print("liballonet_av loaded with libavcodec support")
+    ffi.load(libDir .. "/liballonet."..dylibext, true)
+    ffi.cdef [[
+    void allo_libav_initialize(void);
+    ]]
+    ffi.C.allo_libav_initialize()
+end
+ 
 Client = require("alloui.client")
 ui = require("alloui.ui")
 class = require('pl.class')
 tablex = require('pl.tablex')
 pretty = require('pl.pretty')
+vec3 = require("modules.vec3")
+mat4 = require("modules.mat4")
+
+ui.App.initialLocation = nil
+ui.VideoSurface.libavAvailable = libav_available
+if arg[3] then
+    local ms = {string.match(arg[3], "([-+\\.%d]+), ([-+\\.%d]+), ([-+\\.%d]+), ([-+\\.%d]+), ([-+\\.%d]+), ([-+\\.%d]+), ([-+\\.%d]+), ([-+\\.%d]+), ([-+\\.%d]+), ([-+\\.%d]+), ([-+\\.%d]+), ([-+\\.%d]+), ([-+\\.%d]+), ([-+\\.%d]+), ([-+\\.%d]+), ([-+\\.%d]+)")}
+    local x, y, z = string.match(arg[3], "([-+\\.%d]+),([-+\\.%d]+),([-+\\.%d]+)")
+    if #ms == 16 then
+        local mn = tablex.map(function(s) return tonumber(s) end, ms)
+        local m = mat4(mn)
+        ui.App.initialLocation = m
+    elseif z then
+        ui.App.initialLocation = mat4.translate(mat4(), mat4(), vec3(tonumber(x), tonumber(y), tonumber(z)))
+    end
+end
 
 -- start app
 require("main")
